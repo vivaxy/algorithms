6ddb31aa2fe7dfaea690531d1429bb65bf70dcc1
vivaxy <xyxuye2007@126.com>
2017-05-07 13:26:54 +0800

    feat(distribute-candies.js): :sparkles:

---
+++ distribute-candies.js
@@ -0,0 +1,52 @@
+/**
+ * @since 2017-05-07 13:11:33
+ * @author vivaxy
+ * @see https://leetcode.com/problems/distribute-candies/
+
+ Given an integer array with even length, where different numbers in this array represent different kinds of candies. Each number means one candy of the corresponding kind. You need to distribute these candies equally in number to brother and sister. Return the maximum number of kinds of candies the sister could gain.
+
+ Example 1:
+ Input: candies = [1,1,2,2,3,3]
+ Output: 3
+ Explanation:
+ There are three different kinds of candies (1, 2 and 3), and two candies for each kind.
+ Optimal distribution: The sister has candies [1,2,3] and the brother has candies [1,2,3], too.
+ The sister has three different kinds of candies.
+ Example 2:
+ Input: candies = [1,1,2,3]
+ Output: 2
+ Explanation: For example, the sister has candies [2,3] and the brother has candies [1,1].
+ The sister has two different kinds of candies, the brother has only one kind of candies.
+ Note:
+
+ The length of the given array is in range [2, 10,000], and will be even.
+ The number in given array is in range [-100,000, 100,000].
+
+ */
+
+/**
+ * @see https://leetcode.com/submissions/detail/102204375/
+ * @param {number[]} candies
+ * @return {number}
+ */
+var distributeCandies = function(candies) {
+    var deduplicateArray = function(array) {
+        var results = [];
+        array.forEach(function(item) {
+            if (results.indexOf(item) === -1) {
+                results.push(item);
+            }
+        });
+        return results;
+    };
+    var deduplicatedCandies = deduplicateArray(candies);
+    if (deduplicatedCandies.length > candies.length / 2) {
+        return candies.length / 2;
+    }
+    return deduplicatedCandies.length;
+};
+
+var expect = require('./lib').expect;
+expect(distributeCandies([1, 1, 2, 2, 3, 3]), 3);
+expect(distributeCandies([1, 1, 2, 3]), 2);
+expect(distributeCandies([0, 0, 14, 0, 10, 0, 0, 0]), 3);
