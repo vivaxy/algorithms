bc0d7f6d76dad417cd9a1121f78c7ea9fa759fe5
vivaxy <xyxuye2007@126.com>
2017-06-03 09:25:04 +0800

    feat(javascript): :sparkles:problems
    convert-bst-to-greater-tree.js

--- javascript/problems/convert-bst-to-greater-tree.js
+++ javascript/problems/convert-bst-to-greater-tree.js
@@ -36,7 +36,7 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var convertBST = function(root) {
+var convertBST1 = function(root) {
     var allNodes = [];
     var traverse = function(node, callback) {
         callback(node.val, node);
@@ -64,9 +64,42 @@ var convertBST = function(root) {
     return root;
 };
 
+/**
+ * @see https://leetcode.com/submissions/detail/104780858/
+ * 深度优先，右子树优先，加上右侧的值
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @typedef {Object} TreeNode
+ * @property {number} val
+ * @property {?TreeNode} left
+ * @property {?TreeNode} right
+ *
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var convertBST2 = function(root) {
+    var sum = 0;
+    var traverse = function(node) {
+        if (!node) {
+            return;
+        }
+        traverse(node.right);
+        sum += node.val;
+        node.val = sum;
+        traverse(node.left);
+    };
+    traverse(root);
+    return root;
+};
+
 var test = require('ava');
 test('convert-bst-to-greater-tree', function(t) {
-    var input = {
+    var input1 = {
         val: 5,
         left: {
             val: 2,
@@ -79,7 +112,34 @@ test('convert-bst-to-greater-tree', function(t) {
             right: null
         }
     };
-    t.deepEqual(convertBST(input), {
+    t.deepEqual(convertBST1(input1), {
+        val: 18,
+        left: {
+            val: 20,
+            left: null,
+            right: null
+        },
+        right: {
+            val: 13,
+            left: null,
+            right: null
+        }
+    });
+
+    var input2 = {
+        val: 5,
+        left: {
+            val: 2,
+            left: null,
+            right: null
+        },
+        right: {
+            val: 13,
+            left: null,
+            right: null
+        }
+    };
+    t.deepEqual(convertBST2(input2), {
         val: 18,
         left: {
             val: 20,
