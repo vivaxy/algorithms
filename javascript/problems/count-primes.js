bb96af2a082a414dd22330f5e6f6035b4185fed6
vivaxy <xyxuye2007@126.com>
2017-06-09 22:19:27 +0800

    feat(javascript): :sparkles:problems
    count-primes.js

---
+++ javascript/problems/count-primes.js
@@ -0,0 +1,37 @@
+/**
+ * @since 2017-06-09 22:06:52
+ * @author vivaxy
+ * @see https://leetcode.com/problems/count-primes/
+
+ Count the number of prime numbers less than a non-negative number, n.
+
+ */
+
+/**
+ * @see https://leetcode.com/submissions/detail/105494255/
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    var index = 2;
+    var results = 0;
+    outer: while (index < n) {
+        var i = 2;
+        while (i <= Math.sqrt(index)) {
+            if (index % i === 0) {
+                index++;
+                continue outer;
+            }
+            i++;
+        }
+        results++;
+        index++;
+    }
+    return results;
+};
+
+var test = require('ava');
+test('main', function(t) {
+    t.is(countPrimes(5), 2);
+    t.is(countPrimes(2), 0);
+});
