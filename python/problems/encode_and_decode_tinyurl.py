e131f942d0341f6e52f79e9a84db78d50c45adb2
vivaxy <xyxuye2007@126.com>
2017-12-07 10:37:31 +0800

    feat(python): :sparkles:encode and decode tinyurl

---
+++ python/problems/encode_and_decode_tinyurl.py
@@ -0,0 +1,44 @@
+"""
+https://leetcode.com/problems/encode-and-decode-tinyurl/description/
+
+https://leetcode.com/submissions/detail/131094266/
+"""
+
+
+class Codec:
+
+    def encode(self, longUrl):
+        """
+        Encodes a URL to a shortened URL.
+
+        :type longUrl: str
+        :rtype: str
+        """
+        return longUrl
+
+    def decode(self, shortUrl):
+        """
+        Decodes a shortened URL to its original URL.
+
+        :type shortUrl: str
+        :rtype: str
+        """
+        return shortUrl
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
+
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        codec = Codec()
+        url1 = 'https://leetcode.com'
+        self.assertEqual(codec.decode(codec.encode(url1)), url1)
+
+
+if __name__ == '__main__':
+    unittest.main()
