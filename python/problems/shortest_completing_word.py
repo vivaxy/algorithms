662cc53bb2dd64706132da0d5c1c5aea79573fc9
vivaxy <xyxuye2007@126.com>
2017-12-23 11:12:44 +0800

    feat(python): :sparkles:shortest completing word

--- python/problems/shortest_completing_word.py
+++ python/problems/shortest_completing_word.py
@@ -1,9 +1,10 @@
 """
 https://leetcode.com/problems/shortest-completing-word/description/
 
-
+https://leetcode.com/submissions/detail/132960422/
 """
 
+import sys
 
 class Solution:
     def shortestCompletingWord(self, licensePlate, words):
@@ -12,7 +13,27 @@ class Solution:
         :type words: List[str]
         :rtype: str
         """
-
+        charDict = dict()
+        for char in licensePlate:
+            if char.isalpha():
+                char = char.lower()
+                if not char in charDict:
+                    charDict[char] = 0
+                charDict[char] += 1
+        resultWord = ''
+        resultWordLen = sys.maxsize
+        for word in words:
+            thisWordOK = True
+            for char in charDict:
+                if word.count(char) < charDict[char]:
+                    thisWordOK = False
+                    break
+            if thisWordOK:
+                wordLen = len(word)
+                if wordLen < resultWordLen:
+                    resultWord = word
+                    resultWordLen = wordLen
+        return resultWord
 
 
 import unittest
@@ -21,7 +42,10 @@ import unittest
 class Test(unittest.TestCase):
     def test(self):
         solution = Solution()
-        self.assertEqual(solution.shortestCompletingWord('', []), '')
+        self.assertEqual(solution.shortestCompletingWord(
+            '1s3 PSt', ["step", "steps", "stripe", "stepple"]), 'steps')
+        self.assertEqual(solution.shortestCompletingWord(
+            '1s3 456', ["looks", "pest", "stew", "show"]), 'pest')
 
 
 if __name__ == '__main__':
