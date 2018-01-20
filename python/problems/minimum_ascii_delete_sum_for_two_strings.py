ed7592c9cb887ba4e90fe9bfb485264700fa3313
vivaxy <xyxuye2007@126.com>
2018-01-20 12:49:41 +0800

    feat(python): :sparkles:minimun ascii delete sum for two strings

--- python/problems/minimum_ascii_delete_sum_for_two_strings.py
+++ python/problems/minimum_ascii_delete_sum_for_two_strings.py
@@ -1,7 +1,7 @@
 """
 https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/description/
 
-
+https://leetcode.com/submissions/detail/136983639/
 """
 
 
@@ -12,6 +12,43 @@ class Solution:
         :type s2: str
         :rtype: int
         """
+        matrix = []
+        firstRows = list(map(ord, s1))
+        firstCols = list(map(ord, s2))
+        for rowIndex, col in enumerate(firstCols):
+            matrixRow = []
+            for colIndex, row in enumerate(firstRows):
+                if rowIndex == 0:
+                    if colIndex == 0:
+                        if row == col:
+                            matrixRow.append(0)
+                        else:
+                            matrixRow.append(row + col)
+                    else:
+                        if row == col:
+                            # 使用左上角的值（累加）
+                            matrixRow.append(sum(firstRows[:colIndex]))
+                        else:
+                            matrixRow.append(matrixRow[colIndex - 1] + row)
+                else:
+                    if colIndex == 0:
+                        if row == col:
+                            # 使用左上角的值（累加）
+                            matrixRow.append(sum(firstCols[:rowIndex]))
+                        else:
+                            matrixRow.append(matrix[rowIndex - 1][colIndex] + col)
+                    else:
+                        if row == col:
+                            matrixRow.append(matrix[rowIndex - 1][colIndex - 1])
+                        else:
+                            topWithCol = matrix[rowIndex - 1][colIndex] + col
+                            leftWithRow = matrixRow[colIndex - 1] + row
+                            if topWithCol > leftWithRow:
+                                matrixRow.append(leftWithRow)
+                            else:
+                                matrixRow.append(topWithCol)
+            matrix.append(matrixRow)
+        return matrix.pop().pop()
 
 
 import unittest
@@ -21,6 +58,8 @@ class Test(unittest.TestCase):
     def test(self):
         solution = Solution()
         self.assertEqual(solution.minimumDeleteSum('sea', 'eat'), 231)
+        self.assertEqual(solution.minimumDeleteSum('delete', 'leet'), 403)
+        self.assertEqual(solution.minimumDeleteSum('a', 'at'), 116)
 
 
 if __name__ == '__main__':
