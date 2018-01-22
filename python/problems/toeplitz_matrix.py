f9d7d99d86702d33d8f25cf1c74031c684df478e
vivaxy <xyxuye2007@126.com>
2018-01-22 11:15:49 +0800

    feat(python): :sparkles:toeplitz matrix

---
+++ python/problems/toeplitz_matrix.py
@@ -0,0 +1,55 @@
+"""
+https://leetcode.com/problems/toeplitz-matrix/description/
+
+https://leetcode.com/submissions/detail/137278039/
+"""
+
+
+class Solution:
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        rowCount = len(matrix)
+        colCount = len(matrix[0])
+        for rowIndex in range(rowCount):
+            col = 0
+            row = rowIndex
+            num = matrix[row][col]
+            col += 1
+            row += 1
+            while row < rowCount and col < colCount:
+                number = matrix[row][col]
+                if num != number:
+                    return False
+                col += 1
+                row += 1
+        for colIndex in range(1, colCount):
+            col = colIndex
+            row = 0
+            num = matrix[row][col]
+            col += 1
+            row += 1
+            while row < rowCount and col < colCount:
+                number = matrix[row][col]
+                if num != number:
+                    return False
+                col += 1
+                row += 1
+        return True
+
+
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        solution = Solution()
+        # self.assertEqual(solution.isToeplitzMatrix(
+        #     [[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]]), True)
+        self.assertEqual(solution.isToeplitzMatrix([[1, 2], [2, 2]]), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
