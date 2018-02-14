8afa6646c909c4118f0016e1c4b8b5fe0121e05c
vivaxy <xyxuye2007@126.com>
2018-02-14 11:39:14 +0800

    feat(python): :sparkles:bulb switcher ii

--- python/README.md
+++ python/README.md
@@ -4,4 +4,4 @@
 
 # Run Single Testcase
 
-`> python3 -m unittest discover -s "problems" -p "${problem}.py"`
+`> python3 -m unittest discover -s "problems" -p "bulb_switcher_ii.py"`
---
+++ python/problems/bulb_switcher_ii.py
@@ -0,0 +1,71 @@
+"""
+https://leetcode.com/problems/bulb-switcher-ii/description/
+
+
+"""
+
+
+class Solution:
+    def flipLights(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        def flipEvery(x, index):
+            return not x
+
+        def flipEven(x, index):
+            if index % 2 == 1:
+                return x
+            return not x
+
+        def flipOdd(x, index):
+            if index % 2 == 1:
+                return not x
+            return x
+
+        def flip3k1(x, index):
+            if (index - 1) % 3 == 0:
+                return not x
+            return x
+        status = set()
+        actions = [flipEvery, flipEven, flipOdd, flip3k1]
+
+        def stringifyLights(lights):
+            st = ''
+            for light in lights:
+                if light:
+                    st += '1'
+                else:
+                    st += '0'
+            return st
+
+        def flipLights(actions):
+            lights = [True] * n
+            for action in actions:
+                lights = list(map(action, lights, list(range(1, len(lights) + 1))))
+            status.add(stringifyLights(lights))
+
+        def traverse(steps):
+            if len(steps) == m:
+                return flipLights(steps)
+            for action in actions:
+                traverse(steps + [action])
+        traverse([])
+        return len(status)
+
+
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        solution = Solution()
+        self.assertEqual(solution.flipLights(3, 1), 4)
+        self.assertEqual(solution.flipLights(2, 1), 3)
+        self.assertEqual(solution.flipLights(1, 1), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
