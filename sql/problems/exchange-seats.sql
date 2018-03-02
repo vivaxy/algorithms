a46d0bbc226b8e7231b22fb3efc5cf9dc625e482
vivaxy <xyxuye2007@126.com>
2018-03-02 10:17:15 +0800

    feat(sql): :sparkles:exchange seats

---
+++ sql/problems/exchange-seats.sql
@@ -0,0 +1,18 @@
+/*
+https://leetcode.com/problems/exchange-seats/description/
+
+https://leetcode.com/submissions/detail/143029079/
+*/
+
+SELECT (
+    CASE
+        WHEN MOD(id, 2) != 0 AND counts != id THEN id + 1
+        WHEN MOD(id, 2) != 0 AND counts = id THEN id
+        ELSE id - 1
+    END
+) AS id, student
+FROM seat, (
+    SELECT COUNT(*) AS counts
+    FROM seat
+) AS seat_counts
+ORDER BY id ASC;
