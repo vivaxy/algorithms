95b8529806de95976e5d73904da89a5fbb4c9685
vivaxy <xyxuye2007@126.com>
2018-03-13 12:51:32 +0800

    feat(python): :sparkles:n queens

--- python/problems/n_queens.py
+++ python/problems/n_queens.py
@@ -1,7 +1,7 @@
 """
 https://leetcode.com/problems/n-queens/description/
 
-
+https://leetcode.com/submissions/detail/144845461/
 """
 
 
@@ -12,7 +12,13 @@ class Solution:
         :rtype: List[List[str]]
         """
         def buildBoard(size):
-            return ['.' * size] * size
+            board = []
+            for rowIndex in range(size):
+                row = []
+                for colIndex in range(size):
+                    row.append('.')
+                board.append(row)
+            return board
 
         def check(board, rowIndex, colIndex):
             # |
@@ -26,7 +32,7 @@ class Solution:
             # / up
             r = rowIndex
             c = colIndex
-            while c < n and r >= 0:
+            while c < n - 1 and r > 0:
                 r -= 1
                 c += 1
                 if board[r][c] == 'Q':
@@ -34,31 +40,63 @@ class Solution:
             # / down
             r = rowIndex
             c = colIndex
-            while r < n and c >= 0:
+            while r < n - 1 and c > 0:
                 r += 1
                 c -= 1
-                if board[r][c] == 'Q':
-                    return False
-            # \ up
-            r = rowIndex
-            c = colIndex
-            while r < n and c < n:
-                r += 1
-                c += 1
                 if board[r][c] == 'Q':
                     return False
             # \ down
             r = rowIndex
             c = colIndex
-            while r >= 0 and c >= 0:
+            while r < n - 1 and c < n - 1:
+                r += 1
+                c += 1
+                if board[r][c] == 'Q':
+                    return False
+            # \ up
+            r = rowIndex
+            c = colIndex
+            while r > 0 and c > 0:
                 r -= 1
                 c -= 1
                 if board[r][c] == 'Q':
                     return False
             return True
 
-        def traverse():
-            pass
+        ans = []
+
+        def traverse(pieces):
+            lastPiece = pieces[len(pieces) - 1]
+            nextRowIndex = lastPiece['rowIndex']
+            nextColIndex = lastPiece['colIndex']
+            board = buildBoard(n)
+            for piece in pieces:
+                if piece != lastPiece:
+                    board[piece['rowIndex']][piece['colIndex']] = 'Q'
+            if len(piece):
+                if not check(board, nextRowIndex, nextColIndex):
+                    return
+            board[nextRowIndex][nextColIndex] = 'Q'
+            if len(pieces) == n:
+                ans.append(board)
+                return
+            while not (nextRowIndex == n - 1 and nextColIndex == n - 1):
+                nextColIndex += 1
+                if nextColIndex > n - 1:
+                    nextColIndex = 0
+                    nextRowIndex += 1
+                traverse(
+                    pieces + [{'rowIndex': nextRowIndex, 'colIndex': nextColIndex}])
+        for rowIndex in range(n):
+            for colIndex in range(n):
+                traverse([{'rowIndex': rowIndex, 'colIndex': colIndex}])
+        outAns = []
+        for an in ans:
+            outAn = []
+            for row in an:
+                outAn.append(''.join(row))
+            outAns.append(outAn)
+        return outAns
 
 
 import unittest
