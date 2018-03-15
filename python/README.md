9db613de02551b217d9128c4cf6398703259932a
vivaxy <xyxuye2007@126.com>
2018-03-15 10:37:09 +0800

    feat(python): :sparkles:rotate string

--- python/README.md
+++ python/README.md
@@ -4,4 +4,4 @@
 
 # Run Single Testcase
 
-`> python3 -m unittest discover -s "problems" -p "all_paths_from_source_to_target.py"`
+`> python3 -m unittest discover -s "problems" -p "rotate_string.py"`
---
+++ python/problems/rotate_string.py
@@ -0,0 +1,35 @@
+"""
+https://leetcode.com/problems/rotate-string/description/
+
+https://leetcode.com/submissions/detail/145178981/
+"""
+
+
+class Solution:
+    def rotateString(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: bool
+        """
+        length = len(A)
+        while length:
+            A = A[1:] + A[0:1]
+            if A == B:
+                return True
+            length -= 1
+        return False
+
+
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        solution = Solution()
+        self.assertEqual(solution.rotateString('abcde', 'cdeab'), True)
+        self.assertEqual(solution.rotateString('abcde', 'abced'), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
