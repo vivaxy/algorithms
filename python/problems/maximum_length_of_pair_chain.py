56ac014a8199efcc59c90366cebc9a6a272bb0bc
vivaxy <xyxuye2007@126.com>
2018-03-21 13:43:23 +0800

    feat(python): :sparkles:maximum length of pair chain

--- python/problems/maximum_length_of_pair_chain.py
+++ python/problems/maximum_length_of_pair_chain.py
@@ -1,11 +1,11 @@
 """
 https://leetcode.com/problems/maximum-length-of-pair-chain/solution/
 
-
+https://leetcode.com/submissions/detail/146198048/
 """
 
 
-class Solution:
+class Solution1:
     def findLongestChain(self, pairs):
         """
         :type pairs: List[List[int]]
@@ -19,6 +19,25 @@ class Solution:
                 if sortedPairs[j][1] < sortedPairs[i][0]:
                     ans[i] = max(ans[i], ans[j] + 1)
         return ans[pairsLen - 1]
+
+
+import operator
+import sys
+
+class Solution:
+    def findLongestChain(self, pairs):
+        """
+        :type pairs: List[List[int]]
+        :rtype: int
+        """
+        sortedPairs = sorted(pairs, key=operator.itemgetter(1))
+        ans = 0
+        cur = -sys.maxsize - 1
+        for x, y in sortedPairs:
+            if cur < x:
+                cur = y
+                ans += 1
+        return ans
 
 
 import unittest
