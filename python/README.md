37de88ec180bb2e4fd421ebd3efd3355e5f9b305
vivaxy <xyxuye2007@126.com>
2018-03-28 09:47:46 +0800

    feat(python): :sparkles:number of lines to write string

--- python/README.md
+++ python/README.md
@@ -4,4 +4,4 @@
 
 # Run Single Testcase
 
-`> python3 -m unittest discover -s "problems" -p "unique_morse_code_words.py"`
+`> python3 -m unittest discover -s "problems" -p "number_of_lines_to_write_string.py"`
---
+++ python/problems/number_of_lines_to_write_string.py
@@ -0,0 +1,46 @@
+"""
+https://leetcode.com/problems/number-of-lines-to-write-string/description/
+
+https://leetcode.com/submissions/detail/147297163/
+"""
+
+
+class Solution:
+    def numberOfLines(self, widths, S):
+        """
+        :type widths: List[int]
+        :type S: str
+        :rtype: List[int]
+        """
+        lines = 0
+        currentLineWidthLeft = 100
+        for char in S:
+            charLen = widths[ord(char) - 97]
+            if currentLineWidthLeft >= charLen:
+                currentLineWidthLeft -= charLen
+            else:
+                # new line
+                lines += 1
+                currentLineWidthLeft = 100 - charLen
+        # last line
+        return [lines + 1, 100 - currentLineWidthLeft]
+
+
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        solution = Solution()
+        # self.assertEqual(solution.numberOfLines([
+        #     10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+        #     10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10
+        # ], 'abcdefghijklmnopqrstuvwxyz'), [3, 60])
+        self.assertEqual(solution.numberOfLines([
+            4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+            10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10
+        ], 'bbbcccdddaaa'), [2, 4])
+
+
+if __name__ == '__main__':
+    unittest.main()
