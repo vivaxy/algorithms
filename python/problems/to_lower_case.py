5ce1e8c3b1774e47568ede1497ca66bc5ca7cce3
vivaxy <xyxuye2007@126.com>
2018-09-14 15:01:58 +0800

    feat(python): :sparkles:to_lower_case

---
+++ python/problems/to_lower_case.py
@@ -0,0 +1,26 @@
+"""
+https://leetcode.com/problems/to-lower-case/description/
+
+https://leetcode.com/submissions/detail/175937795/
+"""
+
+class Solution:
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return str.lower()
+
+
+import unittest
+
+class Test(unittest.TestCase):
+    def test(self):
+        solution = Solution()
+        self.assertEqual(solution.toLowerCase('Hello'), 'hello')
+        self.assertEqual(solution.toLowerCase('here'), 'here')
+        self.assertEqual(solution.toLowerCase('LOVELY'), 'lovely')
+
+if __name__ == '__main__':
+    unittest.main()
