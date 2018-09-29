85f7e7bf0d8f2546322670a8c0a132bf11339398
vivaxy <xyxuye2007@126.com>
2018-09-29 10:39:02 +0800

    feat(python): :sparkles:n_ary_tree_postorder_traversal

---
+++ python/problems/n_ary_tree_postorder_traversal.py
@@ -0,0 +1,44 @@
+"""
+https://leetcode.com/problems/n-ary-tree-postorder-traversal/description/
+
+https://leetcode.com/submissions/detail/179182067/
+"""
+
+
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+
+
+class Solution(object):
+    def postorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if root == None:
+            return []
+        ans = []
+        for child in root.children:
+            ans = ans + self.postorder(child)
+        return ans + [root.val]
+
+
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        solution = Solution()
+        rootNode1 = Node(1, [])
+        rootNode1.children.append(Node(3, []))
+        rootNode1.children.append(Node(2, []))
+        rootNode1.children.append(Node(4, []))
+        rootNode1.children[0].children.append(Node(5, []))
+        rootNode1.children[0].children.append(Node(6, []))
+        self.assertEqual(solution.postorder(rootNode1), [5, 6, 3, 2, 4, 1])
+
+
+if __name__ == '__main__':
+    unittest.main()
