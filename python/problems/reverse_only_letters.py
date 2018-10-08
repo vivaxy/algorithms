f758ef2add71d566e38c30477dbe037c2b369183
vivaxy <xyxuye2007@126.com>
2018-10-08 10:34:00 +0800

    feat(python): :sparkles:reverse_only_letters

---
+++ python/problems/reverse_only_letters.py
@@ -0,0 +1,45 @@
+"""
+https://leetcode.com/problems/reverse-only-letters/description/
+
+https://leetcode.com/submissions/detail/181196020/
+"""
+
+
+class Solution:
+    def reverseOnlyLetters(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        asciiLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+        letters = ''
+        for i in range(len(S) - 1, -1, -1):
+            char = S[i]
+            if char in asciiLetters:
+                letters += char
+        i = 0
+        ans = ''
+        for char in S:
+            if char in asciiLetters:
+                ans += letters[i]
+                i += 1
+            else:
+                ans += char
+        return ans
+
+
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        solution = Solution()
+        self.assertEqual(solution.reverseOnlyLetters('ab-cd'), 'dc-ba')
+        self.assertEqual(solution.reverseOnlyLetters(
+            'a-bC-dEf-ghIj'), 'j-Ih-gfE-dCba')
+        self.assertEqual(solution.reverseOnlyLetters(
+            'Test1ng-Leet=code-Q!'), 'Qedo1ct-eeLg=ntse-T!')
+
+
+if __name__ == '__main__':
+    unittest.main()
