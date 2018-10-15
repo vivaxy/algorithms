eabff1026c31ff17001839ab06e7d1b94616a718
vivaxy <xyxuye2007@126.com>
2018-10-15 15:41:43 +0800

    feat(python): :sparkles:minimum_add_to_make_parentheses_valid

---
+++ python/problems/minimum_add_to_make_parentheses_valid.py
@@ -0,0 +1,40 @@
+"""
+https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/description/
+
+https://leetcode.com/submissions/detail/182907873/
+"""
+
+
+class Solution:
+    def minAddToMakeValid(self, S):
+        """
+        :type S: str
+        :rtype: int
+        """
+        ans = 0
+        depth = 0
+        for char in S:
+            if char == '(':
+                depth += 1
+            else:
+                depth -= 1
+                if depth < 0:
+                    depth += 1
+                    ans += 1
+        return ans + depth
+
+
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        solution = Solution()
+        self.assertEqual(solution.minAddToMakeValid("())"), 1)
+        self.assertEqual(solution.minAddToMakeValid("((("), 3)
+        self.assertEqual(solution.minAddToMakeValid("()"), 0)
+        self.assertEqual(solution.minAddToMakeValid("()))(("), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
