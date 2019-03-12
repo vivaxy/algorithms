f788ddccd719166454d30eb93f7a670962eb6b5b
vivaxy <xyxuye2007@126.com>
2019-03-12 10:07:48 +0800

    feat(python): :sparkles:construct_binary_search_tree_from_preorder_traversal

--- python/problems/construct_binary_search_tree_from_preorder_traversal.py
+++ python/problems/construct_binary_search_tree_from_preorder_traversal.py
@@ -1,11 +1,13 @@
+import unittest
 """
 https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
 
-
+https://leetcode.com/submissions/detail/214138493/
 """
 
 from typing import List
 from common.tree_node import TreeNode
+from common.tree_node_to_list import treeNodeToList
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -17,24 +19,30 @@ from common.tree_node import TreeNode
 
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
-        ans = None
-        if len(preorder):
-            ans = TreeNode(preorder[0])
+        if len(preorder) == 0:
+            return None
+        if len(preorder) == 1:
+            return TreeNode(preorder[0])
+        ans = TreeNode(preorder[0])
+        idx = 1
         for i in range(1, len(preorder)):
+            idx = i
             if preorder[i] > preorder[0]:
-                ans.left = self.bstFromPreorder(preorder[1:i])
-                ans.right = self.bstFromPreorder(preorder[i:])
-            break
+                break
+        if preorder[idx] < preorder[0]:
+            idx += 1
+        ans.left = self.bstFromPreorder(preorder[1:idx])
+        ans.right = self.bstFromPreorder(preorder[idx:])
         return ans
-
-
-import unittest
 
 
 class Test(unittest.TestCase):
     def test(self):
         solution = Solution()
-        self.assertEqual(solution.bstFromPreorder([]), [])
+        self.assertEqual(treeNodeToList(solution.bstFromPreorder(
+            [8, 5, 1, 7, 10, 12])), [8, 5, 10, 1, 7, None, 12])
+        self.assertEqual(treeNodeToList(
+            solution.bstFromPreorder([4, 2])), [4, 2])
 
 
 if __name__ == '__main__':
